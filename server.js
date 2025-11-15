f59043ae5ca3d50137be7eb6c8a6e12ea3575ec4
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-15 22:40:31 +0530

    connect to DB

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express = require('express');
+import express from 'express';
+import db from './config/db.js';
+
+
 const app = express();
 const PORT = 3000;
 
@@ -6,6 +9,10 @@ const PORT = 3000;
 app.use(express.json());
 
 
+db.query('SELECT 1', (err, results) => {
+  if (err) console.error('DB test query failed:', err);
+  else console.log('DB connected and test query ran');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
