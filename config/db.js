95a7328dbf53117df39b2741b46a41d4f575ec86
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-16 10:30:33 +0530

    Run automatically sql script when stating the project

--- config/db.js
+++ config/db.js
@@ -1,12 +1,24 @@
+import fs from 'fs';
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  multipleStatements: true
+});
+
+const sql = fs.readFileSync("./config/db.sql", "utf8");
+
+connection.query(sql, (err) => {
+  if (err) {
+    console.error("Error running SQL script:", err);
+  } else {
+    console.log("Database initialized!");
+  }
 });
 
 connection.connect(err => {
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import db from './config/db.js';
 
 
 const app = express();
@@ -7,12 +6,6 @@ const PORT = 3000;
 
 
 app.use(express.json());
-
-
-db.query('SELECT 1', (err, results) => {
-  if (err) console.error('DB test query failed:', err);
-  else console.log('DB connected and test query ran');
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
